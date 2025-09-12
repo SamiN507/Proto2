57109164c3b32260b6098ca34e57d6e2a1eb6b2d
helmeridev <s2300886@edu.bc.fi>
2025-09-12 12:07:14 +0300

    Animation update
    +Added idle animation
    +Made the leuka animation activate on click

---
+++ Assets/Scripts/OnClickLeuka.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class OnClickLeuka : MonoBehaviour
+{
+
+        private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        if (animator == null)
+            Debug.LogWarning("No Animator attached to " + gameObject.name);
+
+        Collider2D collider = GetComponent<Collider2D>();
+        if (collider == null)
+            Debug.LogWarning("No Collider2D attached to " + gameObject.name);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+            if (hit.collider != null && hit.collider.gameObject == gameObject)
+            {
+                Debug.Log("Clicked via Raycast!");
+                animator.SetTrigger("PlayAnim");
+            }
+        }
+    }
+
+    void OnMouseDown()
+        {
+            Debug.Log("Clicked!");
+            animator.SetTrigger("PlayAnim");
+        }
+    
+}
