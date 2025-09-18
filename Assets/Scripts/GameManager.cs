1367d1b5f954b6aaff09fa245a260ec6cf6dde4e
Lurppino <luka.ulmanen@gmail.com>
2025-09-18 10:57:42 +0300

    rmeove money from gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,24 +4,20 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
     public int points;
-    public int money;
     public int health = 100;
 
     public TextMeshProUGUI pointsText;
-    public TextMeshProUGUI moneyText;
     public TextMeshProUGUI healthText;
 
     public void AddPoint()
     {
         points++;
-        money++;
         UpdateUI();
     }
 
     void UpdateUI()
     {
         if (pointsText != null) pointsText.text = $"Pisteet: {points}";
-        if (moneyText != null) moneyText.text = $"Raha: {money}";
         if (healthText != null) healthText.text = $"Terveys: {health}";
     }
 }
