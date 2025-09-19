2bc77af6243ac1113802eac3acf0837c0f02097f
Sami Nissinen <s2300890@edu.bc.fi>
2025-09-19 12:39:27 +0300

    Updated MenuHandler

--- Assets/Scripts/Menu/MenuHandler.cs
+++ Assets/Scripts/Menu/MenuHandler.cs
@@ -3,11 +3,29 @@ using UnityEngine.SceneManagement;
 
 public class MenuHandler : MonoBehaviour
 {
+    [SerializeField] private GameObject gameMenu;
     public static void StartGame () {
         SceneManager.LoadScene("MainGame");
     }
-    public static void ReturnGame()
+    public void ReturnGame()
+    {
+        HideMenu();
+        SceneManager.SetActiveScene(SceneManager.GetActiveScene());
+    }
+    public static void returnMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public static void EndGame()
     {
 
+    }
+    public void ShowMenu()
+    {
+        gameMenu.SetActive(true);
+    }
+    public void HideMenu()
+    {
+        gameMenu.SetActive(false);
     }
 }
