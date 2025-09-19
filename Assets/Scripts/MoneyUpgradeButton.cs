1b8d88f7bef833a03e4ed06d26b08f80f3f4106a
Lurppino <luka.ulmanen@gmail.com>
2025-09-19 10:22:47 +0300

    added money upgrade button and system

---
+++ Assets/Scripts/MoneyUpgradeButton.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MoneyUpgradeButton : MonoBehaviour
+{
+    public GameManager gameManager;
+    public int cost = 100;
+    public int extraMoneyPerClick = 1;
+    public Button button;
+    public TextMeshProUGUI costText;
+
+    void Start()
+    {
+        if (button != null)
+            button.onClick.AddListener(BuyUpgrade);
+
+        UpdateCostText();
+    }
+
+    void BuyUpgrade()
+    {
+        if (gameManager.money >= cost)
+        {
+            gameManager.money -= cost;
+            gameManager.UpgradeMoney(extraMoneyPerClick);
+            gameManager.AddPoint();
+
+            cost += 50;
+            UpdateCostText();
+
+            Debug.Log($"Money per click upgraded! New value: {gameManager.moneyPerClick}");
+        }
+        else
+        {
+            Debug.Log("Liian köyhä!");
+        }
+    }
+
+    void UpdateCostText()
+    {
+        if (costText != null)
+            costText.text = $"MassiKeisari Pasi: {cost}€";
+    }
+}
